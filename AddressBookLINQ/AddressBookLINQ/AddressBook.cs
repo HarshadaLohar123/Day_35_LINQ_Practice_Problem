9f06ff69a0c4c27e437683841604038cb7492947
harshadalohar123 <harshalohar123@gmail.com>
2022-04-29 15:23:45 +0530

    [Harshada]Added:Address book table

---
+++ AddressBookLINQ/AddressBookLINQ/AddressBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookLINQ
+{
+    public class AddressBook
+    {
+        // UC 1 Create a new DataTable
+        DataTable table = new DataTable("AddressBook");
+    }
+}
---
+++ AddressBookLINQ/AddressBookLINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookLINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the LINQ Program ");
+        }
+    }
+}
